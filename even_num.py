c4a0c26b6c04da6896091d6e3028b05aaffacf11
Faraz Tariq <faraztariq05@gmail.com>
2025-04-28 22:21:30 -0400

    first commit

---
+++ even_num.py
@@ -0,0 +1,13 @@
+def even_num(x):
+    lst = []
+    for i in range(len(x)):
+        if x[i] % 2 == 0:
+            lst.append(x[i])
+    print(lst)
+    return lst
+
+if __name__ == '__main__':
+    x = [1,2,3,4,5,6]
+    assert even_num(x) == [2,4,6]
+
+    
